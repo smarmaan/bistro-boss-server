ace665ced4a00eeefd2e8b51dd09090509f6d189
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-04 17:27:07 +0600

    api created to patch user data to make admin...

--- index.js
+++ index.js
@@ -53,6 +53,22 @@ async function run() {
       res.send(result);
     });
 
+    app.patch("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const filter = { _id: new ObjectId(id) };
+
+      const updateDoc = {
+        $set: {
+          role: "admin",
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+
+      res.send(result);
+    });
+
     // menu related api methods
 
     app.get("/menu", async (req, res) => {
