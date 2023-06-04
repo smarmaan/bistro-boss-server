db6157633eba73e082f8e421b4e627cd8c1f62c0
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-04 22:03:10 +0600

    admin user delete function added api

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
-console.log();
-
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2qbsssi.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -53,11 +51,11 @@ async function run() {
       res.send(result);
     });
 
+    //   admin related api methods
+
     app.patch("/users/admin/:id", async (req, res) => {
       const id = req.params.id;
-
       const filter = { _id: new ObjectId(id) };
-
       const updateDoc = {
         $set: {
           role: "admin",
@@ -65,7 +63,13 @@ async function run() {
       };
 
       const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
+    app.delete("/users/admin/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
       res.send(result);
     });
 
