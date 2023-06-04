7312f2654f59343b79af5b161561c4007d4c8e36
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-04 09:11:25 +0600

    api created for delete cart products

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.json());
 
 console.log();
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2qbsssi.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -41,11 +41,35 @@ async function run() {
 
     // cart collection
 
+    app.get("/carts", async (req, res) => {
+      const email = req.query.email;
+      console.log(email);
+
+      if (!email) {
+        res.send([]);
+      }
+
+      const query = { email: email };
+
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
+
     app.post("/carts", async (req, res) => {
       const item = req.body;
       console.log(item);
 
       const result = await cartCollection.insertOne(item);
+      res.send(result);
+    });
+
+    app.delete("/carts/:id", async (req, res) => {
+      const id = req.params.id;
+
+      const query = { _id: new ObjectId(id) };
+
+      const result = await cartCollection.deleteOne(query);
+
       res.send(result);
     });
 
