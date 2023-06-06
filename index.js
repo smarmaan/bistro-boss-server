c203379f988725057d2928cabc5a7977441e2471
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-06 09:21:31 +0600

    post api created for admin to add item in db

--- index.js
+++ index.js
@@ -144,6 +144,12 @@ async function run() {
       res.send(result);
     });
 
+    app.post("/menu", verifyJWT, verifyAdmin, async (req, res) => {
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    });
+
     // reviews related api methods
 
     app.get("/reviews", async (req, res) => {
