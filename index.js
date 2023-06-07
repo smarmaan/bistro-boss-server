212646996083959b0dc16fc0fbde7af29c46c175
S M Armaan Hossain <armaanhossainirtaza7@gmail.com>
2023-06-07 14:51:17 +0600

    admin-stats route set up

--- index.js
+++ index.js
@@ -247,6 +247,19 @@ async function run() {
       res.send({ insertResult, deleteResult });
     });
 
+    //  dashboard info...
+
+    app.get("/admin-stats", verifyJWT, verifyAdmin, async (req, res) => {
+      const users = await usersCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce((sum, payment) => sum + payment.price, 0);
+
+      res.send({ revenue, users, products, orders });
+    });
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
